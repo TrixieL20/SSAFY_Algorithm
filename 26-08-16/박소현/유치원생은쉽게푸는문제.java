420d432054e7bc8f8aa2fbb7c0dbb1b2fed3d1ce
Sohyun Park <37572029+piche32@users.noreply.github.com>
2026-08-15 11:30:18 +0900

    fix: 유치원생은 쉽게 푸는 문제 출력 개선

--- 26-08-16/박소현/유치원생은쉽게푸는문제.java
+++ 26-08-16/박소현/유치원생은쉽게푸는문제.java
@@ -6,15 +6,14 @@ public class Solution {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		for(int testCase = 1; testCase <= T; testCase++)
 		{
 			int N = Integer.parseInt(br.readLine());
 			int repeat = N / 2;
-			StringBuilder sb = new StringBuilder();
-            if(N == 1)
+			if(N == 1)
             {
 				sb.append(0).append('\n');
-				System.out.print(sb);
                 continue;
             }
             if(N % 2 == 1)
@@ -27,7 +26,7 @@ public class Solution {
 			}
 			
 			sb.append('\n');
-			System.out.print(sb);
 		}
+		System.out.print(sb);
 	}
 }
