646212dff015b739b662bb422fa6efcb14eb996b
minjung <minjung0768@gmail.com>
2026-08-16 22:37:49 +0900

    docs: 거리두기 확인하기 변수명 변경

--- 26-08-09/이민정/거리두기확인하기_이민정.java
+++ 26-08-09/이민정/거리두기확인하기_이민정.java
@@ -26,9 +26,9 @@ class Solution {
         return 1;
     }
 
-    public boolean isManhattan(int y, int x, String[] places) {
+    public boolean isManhattan(int y, int x, String[] place) {
         Queue<int[]> queue = new ArrayDeque<>();
-        int[][] visited = new int[places.length][places[0].length()];
+        int[][] visited = new int[place.length][place[0].length()];
 
         queue.add(new int[]{y, x});
         visited[y][x] = 1;
@@ -43,8 +43,8 @@ class Solution {
             for (int i = 0; i < 4; i++) {
                 int cy = y + dy[i], cx = x + dx[i];
 
-                if (cy < 0 || cy >= 5 || cx < 0 || cx >= 5 || places[cy].charAt(cx) == 'X' || visited[cy][cx] > 0) continue;
-                else if (places[cy].charAt(cx) == 'P') return false;
+                if (cy < 0 || cy >= 5 || cx < 0 || cx >= 5 || place[cy].charAt(cx) == 'X' || visited[cy][cx] > 0) continue;
+                else if (place[cy].charAt(cx) == 'P') return false;
 
                 queue.add(new int[]{cy, cx});
                 visited[cy][cx] = visited[y][x] + 1;
