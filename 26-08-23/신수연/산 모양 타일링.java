0da3eb3da422e642149b113d5a5dfe20f8555208
SuyeonShinnnn <127740774+SuyeonShinnnn@users.noreply.github.com>
2026-08-26 12:39:07 +0900

    코드 개선

--- 26-08-23/신수연/산 모양 타일링.java
+++ 26-08-23/신수연/산 모양 타일링.java
@@ -1,4 +1,5 @@
 class Solution {
+    private final int MOD = 10007;
     public int solution(int n, int[] tops) {
         int[][] dp = new int[n + 1][2];
 
@@ -8,18 +9,18 @@ class Solution {
         for (int i = 1; i <= n; i++) {
 
             // 오른쪽 아래 삼각형이 마름모에 포함되는 경우
-            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % 10007;
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
 
             // 현재 위쪽 삼각형이 있는지에 따라 경우의 수가 달라짐
             if (tops[i - 1] == 1) {
                 dp[i][1] =
-                    (2 * dp[i - 1][0] + 3 * dp[i - 1][1]) % 10007;
+                    (2 * dp[i - 1][0] + 3 * dp[i - 1][1]) % MOD;
             } else {
                 dp[i][1] =
-                    (dp[i - 1][0] + 2 * dp[i - 1][1]) % 10007;
+                    (dp[i - 1][0] + 2 * dp[i - 1][1]) % MOD;
             }
         }
 
-        return (dp[n][0] + dp[n][1]) % 10007;
+        return (dp[n][0] + dp[n][1]) % MOD;
     }
 }
