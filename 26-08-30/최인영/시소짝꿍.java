fa4995289fa929221d5c7e4db7fdc39ebfea8c0c
1ny0ung <0828_iyiy@naver.com>
2026-08-30 11:08:55 +0900

    🎉시소짝꿍 문제 풀이

--- 26-08-30/최인영/시소짝꿍.java
+++ 26-08-30/최인영/시소짝꿍.java
@@ -8,23 +8,23 @@ class Solution {
         // (몸무게, 인원수) 저장하는 hashmap 
         Map<Integer, Integer> weightCount = new HashMap<>();
         for(int w : weights){
-            answer += weightCount.getOrDefault(w, 0) + 1;
-            weightCount.put(w, weightCount.getOrDefault(w,0) + 1);
+            // 몸무게 w = weightCounts인 경우 세기 
+            answer += weightCount.getOrDefault(w, 0);
+            // w = weightCounts * 2인 경우 세기 
+            if(w % 2 == 0){
+                answer += weightCount.getOrDefault(w / 2, 0);
+            }
+            // w * 2 = weightCounts * 3인 경우 세기 
+            if(2 * w % 3 == 0){
+                answer += weightCount.getOrDefault(w * 2 / 3, 0);
+            }
+            // w * 3 = weightCounts * 4인 경우 세기 
+            if(3 * w % 4 == 0){
+                answer += weightCount.getOrDefault(w * 3 / 4, 0);
+            }
+            // 해당 weight도 weightCount에 추가
+            weightCount.put(w, weightCount.getOrDefault(w, 0) + 1);
         }
         return answer; 
-    }
-
-    // 매개변수로 들어온 weight에 대해 2, 3, 4를 곱해 시소에 걸리는 토크의 크기 만듦
-    // -> sisoCount에 중복 key값(이전에 동일 토크 가해진 적 있는지 체크)
-    // -> 없었다면 새로 추가, 있었다면 해당 토크를 key로 가지는 value를 +1
-    Boolean checkWeight(List<Integer> torques, int weight){
-        int[] dist = {2, 3, 4};
-        for(int d: dist){
-            if(torques.contains(d * weight)){ return true; }
-            else{
-                torques.add(d * weight);
-            }
-        }
-        return false;
     }
 }
