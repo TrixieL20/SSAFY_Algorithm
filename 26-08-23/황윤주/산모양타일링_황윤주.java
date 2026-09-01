cff4aecedca46d36194763ef68154b2b12886185
hwangyun <117272047+yunju-hwang@users.noreply.github.com>
2026-09-01 10:29:28 +0900

    26/08/26 황윤주 문제 풀이 (#27)

---
+++ 26-08-23/황윤주/산모양타일링_황윤주.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    static final int MOD = 10007;
+
+    public int solution(int n, int[] tops) {
+
+        long[][] dp = new long[n][2];
+
+        // 첫 번째 열
+        if (tops[0] == 1) {
+            dp[0][0] = 3;
+            dp[0][1] = 1;
+        } else {
+            dp[0][0] = 2;
+            dp[0][1] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            if (tops[i] == 1) {
+
+                dp[i][0] =
+                        (dp[i - 1][0] * 3
+                                + dp[i - 1][1] * 2)
+                                % MOD;
+
+            } else {
+
+                dp[i][0] =
+                        (dp[i - 1][0] * 2
+                                + dp[i - 1][1])
+                                % MOD;
+            }
+
+            dp[i][1] =
+                    (dp[i - 1][0]
+                            + dp[i - 1][1])
+                            % MOD;
+        }
+
+        return (int)
+                ((dp[n - 1][0]
+                        + dp[n - 1][1])
+                        % MOD);
+    }
+}
