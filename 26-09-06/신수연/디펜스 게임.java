0ba9f2290d06ca2255a000d8cb99a9687432841a
SuyeonShinnnn <127740774+SuyeonShinnnn@users.noreply.github.com>
2026-09-07 17:35:04 +0900

    주석추가

--- 26-09-06/신수연/디펜스 게임.java
+++ 26-09-06/신수연/디펜스 게임.java
@@ -9,11 +9,14 @@ class Solution {
             n -= e;
             
             pq.offer(e);
-            
+
+            // n이 0보다 작고 무적권 k가 0보다 클 때
+            // 무적권 사용
             if(n < 0 && k > 0) {
                 n += pq.poll();
                 k--;
             }
+            // n이 0보다 작고 무적권도 모두 사용했을 때 라운드를 카운트한 answer 리턴
             else if(n < 0 && k == 0) return answer;
             
             answer++;
