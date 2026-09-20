289ce099c5679a144df81c1b3863098dce5fe719
TrixieL20 <minjung0768@gmail.com>
2026-09-21 03:20:03 +0900

    26/09/18 이민정 문제 풀이 (#46)
    * docs: 양궁대회 풀이 완료
    
    * docs: 문제 풀이 완료
    
    * Update 햄버거다이어트.java
    
    * docs: 안 쓰이는 배열 선언 코드 삭제
    
    * docs: 잘못된 구문 수정
    
    Refine explanations regarding tie conditions and optimization strategies.
    
    * docs: Refine time complexity and DFS handling in archery.md
    
    Updated time complexity explanation and clarified DFS logic for archery score calculation.

---
+++ 26-09-18/이민정/햄버거다이어트.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+    static int[][] ingredients = null;
+    static int n = 0, l = 0, answer = 0;
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T;
+        T = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            n = Integer.parseInt(st.nextToken());
+            l = Integer.parseInt(st.nextToken());
+
+            ingredients = new int[n][2];
+
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+
+                ingredients[i][0] = Integer.parseInt(st.nextToken()); // 맛
+                ingredients[i][1] = Integer.parseInt(st.nextToken()); // 칼로리
+            }
+
+            answer = 0;
+
+            dfs(0, 0, l);
+
+            sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int score, int idx, int leftCalorie) {
+        if (idx == n) {
+            answer = Math.max(answer, score); return;
+        }
+
+        int taste = ingredients[idx][0];
+        int calorie = ingredients[idx][1];
+
+        if (calorie <= leftCalorie) {
+            dfs(score + taste, idx + 1, leftCalorie - calorie);
+        }
+
+        dfs(score, idx + 1, leftCalorie);
+    }
+}
